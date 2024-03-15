411b29aa397926d163e0d22998c653e741089c32
inno-asiimwe <asiimwe@outlook.com>
2024-03-15 08:49:37 +0300

    Add minimal terraform configuration for GKE

---
+++ terraform/main.tf
@@ -0,0 +1,10 @@
+// Configure the Google Cloud Provider
+provider "google" {
+    project = "cloud-computing-demo-417220"
+    region = "us-central1"
+}
+
+
+
+// Configure Terraform Backend
+terraform {}
